fa2149c4c78e66baf301ac16e3c75e9d9602bbda
Meruyert Aitbayeva <aitbayev@usc.edu>
2013-11-17 18:23:58 -0800

    mock

---
+++ src/simcity/tests/Mock/Mock.java
@@ -0,0 +1,28 @@
+package simcity.tests.mock;
+
+import java.io.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+/**
+ * This is the base class for all mocks.
+ *
+ * @author Nikhil Bedi
+ *
+ */
+public class Mock {
+	protected String name;
+
+	public Mock(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String toString() {
+		return this.getClass().getName() + ": " + name;
+	}
+
+}
