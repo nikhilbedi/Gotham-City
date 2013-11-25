6decaa93c5600be8383a2bba809f27c6e957ce36
BriceRoland <Broland@usc.edu>
2013-11-24 18:40:05 -0800

    added greetertest and updated some spacing

--- src/simcity/PersonAgent.java
+++ src/simcity/PersonAgent.java
@@ -538,7 +538,7 @@ public class PersonAgent extends Agent implements Person {
 			}
 
 			//Let me even see if I got money..
-			if(accountNumber == 0 || moneyState == MoneyState.Low || moneyState == MoneyState.High) {
+			if(/*accountNumber == 0 ||*/ moneyState == MoneyState.Low || moneyState == MoneyState.High) {
 				if(currentBuilding != bank){
 					goToBank();
 					return true;
