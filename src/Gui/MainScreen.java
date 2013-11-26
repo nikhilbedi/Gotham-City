2a9199aeca0612e240acc127877dab577f1cb806
William Hunter McNichols <wmnicho@usc.edu>
2013-11-26 01:25:20 -0800

    added multiple houses and restaurants to city screen. Some hacky stuff but mostly supported.

--- src/Gui/MainScreen.java
+++ src/Gui/MainScreen.java
@@ -26,28 +26,72 @@ public class MainScreen extends Screen{
 
 	ArrayList<Building> buildings = new ArrayList<Building>();
 	Building house;
+	Building house2,house3,house4,house5,house6;
 	Building market;
 	Building bank;
 	Building rest1;
+	Building rest2;
+	Building rest3;
+	Building rest4;
+	Building rest5;
 
 
 
 	public MainScreen()
 	{
 		temp = 1;
+		
+		
 		house = new Home("Home", 390, 590, 400, 600);
 		house.setImagePath("/resources/Buildings/HouseDark2.png");
-		market = new Market("Market", 210, 110, 200, 100);
+		
+		//These are just added to make Evan happy
+		house2 = new Home("Home2", 90, 590, 100, 600);
+		house2.setImagePath("/resources/Buildings/HouseDark2.png");
+		house3 = new Home("Home3", 190, 590, 200, 600);
+		house3.setImagePath("/resources/Buildings/HouseDark2.png");
+		house4 = new Home("Home4", 290, 590, 300, 600);
+		house4.setImagePath("/resources/Buildings/HouseDark2.png");
+		house5 = new Home("Home5", 490, 590, 500, 600);
+		house5.setImagePath("/resources/Buildings/HouseDark2.png");
+		house6 = new Home("Home6", 590, 590, 600, 600);
+		house6.setImagePath("/resources/Buildings/HouseDark2.png");
+		
+		
+		market = new Market("Market", 500, 100, 500, 100);
 		market.setImagePath("/resources/Buildings/MarketDark2.png");
-		bank = new Bank("Bank", 410, 110, 400, 100);
+		bank = new Bank("Bank", 350, 100, 350, 100);
 		bank.setImagePath("/resources/Buildings/BankDark2.png");
-		rest1 = new Restaurant1("Restaurant 1", 610, 110, 600, 100);
+		
+		rest1 = new Restaurant1("Restaurant 1", 200, 100, 200, 100);
 		rest1.setImagePath("/resources/Buildings/RestaurantDark2.png");
+		rest2 = new Restaurant1("Restaurant 2", 50, 200, 50, 200);
+		rest2.setImagePath("/resources/Buildings/RestaurantDark2.png");
+		rest3 = new Restaurant1("Restaurant 3", 650, 200, 650, 200);
+		rest3.setImagePath("/resources/Buildings/RestaurantDark2.png");
+		rest4 = new Restaurant1("Restaurant 4", 50, 400, 50, 400);
+		rest4.setImagePath("/resources/Buildings/RestaurantDark2.png");
+		rest5 = new Restaurant1("Restaurant 5", 650, 400, 650, 400);
+		rest5.setImagePath("/resources/Buildings/RestaurantDark2.png");
+		
 
 		buildings.add(house);
 		buildings.add(market);
 		buildings.add(bank);
 		buildings.add(rest1);
+		buildings.add(rest2);
+		buildings.add(rest3);
+		buildings.add(rest4);
+		buildings.add(rest5);
+		
+		//just for looks
+		buildings.add(house2);
+		buildings.add(house3);
+		buildings.add(house4);
+		buildings.add(house5);
+		buildings.add(house6);
+		
+		
 	}
 
 	public  void paintBackground(Graphics g)
@@ -69,7 +113,11 @@ public class MainScreen extends Screen{
 	public List<Restaurant> getRestaurantList(){
 		List<Restaurant> tempcast = new ArrayList<Restaurant>();
 		tempcast.add((Restaurant1)rest1);
-
+		tempcast.add((Restaurant1)rest2);
+		tempcast.add((Restaurant1)rest3);
+		tempcast.add((Restaurant1)rest4);
+		tempcast.add((Restaurant1)rest5);
+		
 		return tempcast;
 	}
 
